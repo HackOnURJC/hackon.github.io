0693f318c8b3083811033419a0fbd0664241f48e
yisusrg05 <56789931+yisusrg05@users.noreply.github.com>
2023-01-31 11:23:30 +0100

    Arreglados logos patrocinadores y charla Airbus

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -48,7 +48,17 @@ window.onclick = function(event) {
   }
 }
 
-$(function(){
-  $("#navbarPrincipal").load("navbarPrincipal.html"); 
-  $("#footer").load("footer.html"); 
-});
+var myArray = ['../assets/img/ponentes/Esperanza_Rodriguez.jpeg', '../assets/img/ponentes/Alberto_Gonzalez.jpeg'];
+
+var count = 0;
+setInterval(function() {
+  //use this below line if you want random images
+  //var rand = myArray[Math.floor(Math.random() * myArray.length)];
+
+  if (count >= myArray.length) count = 0; // if it is last image then show the first image.
+  // use this below line if you want images in order.
+  var rand = myArray[count];
+  document.getElementById('mainPhoto').src = rand;
+  document.getElementById('mainPhoto').alt = rand; // use 'alt' to display the image name if image is not found
+  count++;
+}, 1000); 
